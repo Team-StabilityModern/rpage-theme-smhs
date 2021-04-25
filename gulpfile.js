c07f2bfa26741104f1baeb1e5ee27f0580d7b065
Yi-Jyun Pan <pan93412@gmail.com>
2021-04-26 00:50:07 +0800

    env(): done

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,25 @@ const gulp = require("gulp");
  */
 exports.css = () => {
   const postcss = require("gulp-postcss");
-  const concat = require('gulp-concat');
+  const concat = require("gulp-concat");
+  const gulpStylelint = require("gulp-stylelint");
+
   return gulp
     .src("src/**/*.css")
-    .pipe(postcss([require("precss"), require("autoprefixer")]))
+    .pipe(
+      gulpStylelint({
+        reporters: [{ formatter: "string", console: true }],
+      })
+    )
+    .pipe(
+      postcss([
+        require("precss"),
+        require("autoprefixer"),
+        require("cssnano")({
+          preset: "default",
+        }),
+      ])
+    )
     .pipe(concat("all.css"))
     .pipe(gulp.dest("dist/"));
 };
